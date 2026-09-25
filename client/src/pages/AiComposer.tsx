b34b0f5391e69947efbb86f91ede16207a2a59ad
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-09-25 12:49:08 +0530

    sidebar completed

---
+++ client/src/pages/AiComposer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AiComposer() {
+  return <div>AiComposer</div>;
+}
+
+export default AiComposer;
---
+++ client/src/pages/Scheduler.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Scheduler() {
+  return <div>Scheduler</div>;
+}
+
+export default Scheduler;
